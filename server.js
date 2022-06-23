f2ae5b6c536f7f11733c37295fd631334cc42083
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-06-22 19:57:46 -0500

    added title property

--- server.js
+++ server.js
@@ -7,36 +7,43 @@ app.use(cors())
 
 const movies = {
     'jurassic park':{
+        'title':'Jurassic Park',
         'releaseYear': 1993,
         'director':'Steven Spielberg',
         'runTime':'127 min',
     },
     'the lost world: jurassic park':{
+        'title':'The Lost World: Jurassic Park',
         'releaseYear': 1997,
         'director':'Steven Spielberg',
         'runTime':'129 min',
     },
     'jurassic park 3':{
+        'title':'Jurassic Park III',
         'releaseYear': 2001,
         'director':'Joe Johnston',
         'runTime':'92 min',
     },
     'jurassic world':{
+        'title':'Jurassic World',
         'releaseYear': 2015,
         'director':'Colin Trevorrow',
         'runTime':'124 min',
     },
     'jurassic world: fallen kingdom':{
+        'title':'Jurassic World: Fallen Kingdom',
         'releaseYear': 2018,
         'director':'J. A. Bayona',
         'runTime':'128 min',
     },
     'jurassic world: dominion':{
+        'title':'Jurassic World: Dominion',
         'releaseYear': 2022,
         'director':'Colin Trevorrow',
         'runTime':'146 min',
     },
     'unknown':{
+        'title':'unknown',
         'releaseYear': NaN,
         'director':'unknown',
         'runTime':'unknown',
